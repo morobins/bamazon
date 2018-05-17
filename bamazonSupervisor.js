1f271a1e7db79e3851138680d528cc5fa0cec726
Monique Robinson <monique.robinson2@gmail.com>
2018-05-17 09:44:25 -0400

    deleted node modules

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -53,6 +53,22 @@ inquirer
 // department_id	department_name	over_head_costs	product_sales	total_profit
 
 // The total_profit column should be calculated on the fly using the difference between over_head_costs and product_sales. total_profit should not be stored in any database. You should use a custom alias.
+
+function viewProductSales() {
+    // Selects a few columns from the departments table, calculates a total_profit column
+    connection.query(
+      "SELECT departProd.department_id, departProd.department_name, departProd.over_head_costs, SUM(departProd.product_sales) as product_sales, (SUM(departProd.product_sales) - departProd.over_head_costs) as total_profit FROM (SELECT departments.department_id, departments.department_name, departments.over_head_costs, IFNULL(products.product_sales, 0) as product_sales FROM products RIGHT JOIN departments ON products.department_name = departments.department_name) as departProd GROUP BY department_id",
+      function(err, res) {
+        console.table(res);
+        superTasks();
+      }
+    );
+  }
+
+
+
+//try this
+//SELECT(SUM(department.over_head_costs) - (product_sales) GROUP BY department_name) AS total_profit FROM departments LEFT JOIN by products WHERE departments.department_name = products.deparment_name
 //SELECT (SUM(dept.overhead)-SUM(dept.sales)) AS total_profit from dept
 //inner join products
 
